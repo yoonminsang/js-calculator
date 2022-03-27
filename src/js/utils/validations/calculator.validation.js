3e960b2616da684efdd9af1cffc45b6f0120e291
yoonminsang <apdlyooapdl@naver.com>
2022-03-27 16:10:46 +0900

    feat: operation validation 함수 생성

--- src/js/utils/validations/calculator.validation.js
+++ src/js/utils/validations/calculator.validation.js
@@ -4,3 +4,8 @@ export const digitLengthValidationFn = (digit) => {
   if (digit?.length >= MAX_DIGITS_LENGTH) return '숫자는 최대 3자리 수까지 입력 가능합니다.';
   return true;
 };
+
+export const operationValidationFn = (digits, operations) => {
+  if (digits.length <= operations.length) return '연산자 이후에는 숫자를 먼저 입력해주세요.';
+  return true;
+};
