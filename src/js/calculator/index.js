4f2dd083909bb51c378bb513b9dc9fd3a5e048b6
yoonminsang <apdlyooapdl@naver.com>
2022-03-27 16:28:25 +0900

    feat: = 연산자 기능 구현 및 불필요한 화살표함수제거

--- src/js/calculator/index.js
+++ src/js/calculator/index.js
@@ -24,19 +24,19 @@ class Calculator {
     $operations.addEventListener('click', this.onClickOperations);
   }
 
-  setState = (nextState) => {
+  setState(nextState) {
     this.state = { ...this.state, ...nextState };
     this.render();
-  };
+  }
 
-  render = () => {
+  render() {
     const { digits, operations } = this.state;
     const totalText = digits.reduce((acc, cur, index) => {
       if (operations[index]) return acc + cur + operations[index];
       return acc + cur;
     }, '');
     this.$total.textContent = totalText || ZERO;
-  };
+  }
 
   onClickDigits = (e) => {
     const { digits, operations } = this.state;
@@ -68,13 +68,31 @@ class Calculator {
     }
 
     if (value === EQUAL) {
-      this.getSum();
+      this.calculateTotal();
     } else {
       this.setState({ operations: [...operations, value] });
     }
   };
 
-  getSum = () => {};
+  calculateTotal() {
+    // TODO: 일단 요구사항에 따라 2개의 숫자만 계산. 추후에 n개의 숫자로 수정
+    const { digits, operations } = this.state;
+    if (operations.length === 0) {
+      return this.setState({ digits: [digits[0]], operations: [] });
+    }
+    switch (operations[0]) {
+      case '+':
+        return this.setState({ digits: [Number(digits[0]) + Number(digits[1])], operations: [] });
+      case '-':
+        return this.setState({ digits: [Number(digits[0]) - Number(digits[1])], operations: [] });
+      case 'X':
+        return this.setState({ digits: [Number(digits[0]) * Number(digits[1])], operations: [] });
+      case '/':
+        return this.setState({ digits: [Math.floor(Number(digits[0]) / Number(digits[1]))], operations: [] });
+      default:
+        console.error('연산자를 확인해주세요.');
+    }
+  }
 }
 
 export default Calculator;
